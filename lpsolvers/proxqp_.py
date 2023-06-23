15a97c5ede73167d32b5d455e691ef0e0eb48c36
Stéphane Caron <stephane.caron@normalesup.org>
2023-06-23 10:28:38 +0200

    Fix references to lpsolvers

--- lpsolvers/proxqp_.py
+++ lpsolvers/proxqp_.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2016-2022 Stéphane Caron and the qpsolvers contributors.
+# Copyright (C) 2016-2022 Stéphane Caron
+# Copyright (C) 2023 Inria
 #
-# This file is part of qpsolvers.
+# This file is part of lpsolvers.
 #
-# qpsolvers is free software: you can redistribute it and/or modify it under
+# lpsolvers is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option) any
 # later version.
 #
-# qpsolvers is distributed in the hope that it will be useful, but WITHOUT ANY
+# lpsolvers is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 # A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
 # details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
+# along with lpsolvers. If not, see <http://www.gnu.org/licenses/>.
 
 """Solver interface for `ProxQP`_.
 
