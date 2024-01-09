2328f945268e44ec46b1b1044ac9ca05d289a3e9
Stéphane Caron <stephane.caron@normalesup.org>
2024-01-09 15:50:02 +0100

    Switch headers to use SPDX license identifiers

--- __init__.py
+++ __init__.py
@@ -1,22 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+# SPDX-License-Identifier: LGPL-3.0-or-later
 # Copyright (C) 2016-2022 Stéphane Caron
-#
-# This file is part of lpsolvers.
-#
-# lpsolvers is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
-#
-# lpsolvers is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with lpsolvers. If not, see <http://www.gnu.org/licenses/>.
 
 """
 This file allows the whole repository to act as a Python module when e.g.
